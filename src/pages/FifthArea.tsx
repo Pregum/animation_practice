f9cc9afa6c2a65d848ed73ee805f3a5a64aa8b01
Pregum <yasushi.hino@i-standard.jp>
2022-05-20 15:37:16 +0900

    wip: ５枚目の配置が概ね終わった

--- src/pages/FifthArea.tsx
+++ src/pages/FifthArea.tsx
@@ -1,6 +1,33 @@
-import { FC, Suspense } from 'react';
+import { FC, Suspense, useEffect, useState } from 'react';
+import { animated, useSpring } from 'react-spring';
 
 const FifthArea: FC = () => {
+  const [screenY, setScreenY] = useState<number>(0);
+  const [h, setH] = useState<number>(30);
+  const [op, setOp] = useState<number>(1);
+
+  const styles = useSpring({
+    to: {
+      height: h,
+      opacity: op,
+    },
+  });
+
+  useEffect(() => {
+    window.addEventListener('wheel', (e) => {
+      // console.log('on wheel....e: ', e.screenY, 'offsetY: ', e.offsetY);
+      const t = e.pageY - e.y;
+      setScreenY(t);
+      if (t > 2800) {
+        setH(20);
+        setOp(0);
+      } else if (t <= 2800) {
+        setH(400);
+        setOp(1);
+      }
+    });
+  }, []);
+
   return (
     <div
       style={{
@@ -9,14 +36,26 @@ const FifthArea: FC = () => {
         width: '100%',
         height: '80vh',
         backgroundColor: '#224444',
+        position: 'relative',
       }}
     >
+      <animated.div
+        style={{
+          position: 'absolute',
+          width: '13vw',
+          left: '75vw',
+          top: 80,
+          backgroundColor: '#999999',
+          height: styles.height,
+          opacity: styles.opacity,
+        }}
+      />
       {/* ここに第２のエリアを挟む */}
       <span
         style={{
-          position: 'relative',
-          top: 120,
-          left: '60vw',
+          position: 'absolute',
+          top: 220,
+          left: '50vw',
           fontSize: 34,
           color: '#ffffff',
         }}
@@ -25,9 +64,9 @@ const FifthArea: FC = () => {
       </span>
       <span
         style={{
-          position: 'relative',
-          top: 180,
-          left: '48vw',
+          position: 'absolute',
+          top: 280,
+          left: '50vw',
           fontSize: 34,
           color: '#ffffff',
         }}
