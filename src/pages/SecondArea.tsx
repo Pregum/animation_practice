1ea22a117b91a691e1994cddb037fa9d553cda06
Pregum <yasushi.hino@i-standard.jp>
2022-05-20 15:20:07 +0900

    wip: 2段目の見た目できた

--- src/pages/SecondArea.tsx
+++ src/pages/SecondArea.tsx
@@ -1,6 +1,32 @@
-import { FC, Suspense } from 'react';
+import { FC, Suspense, useEffect, useState } from 'react';
+import { animated, useSpring } from 'react-spring';
 
 const SecondArea: FC = () => {
+  const [screenY, setScreenY] = useState<number>(0);
+  const [h, setH] = useState<number>(30);
+  const [op, setOp] = useState<number>(1);
+
+  const styles = useSpring({
+    to: {
+      height: h,
+      opacity: op,
+    },
+  });
+
+  useEffect(() => {
+    window.addEventListener('wheel', (e) => {
+      // console.log('on wheel....e: ', e.screenY, 'offsetY: ', e.offsetY);
+      const t = e.pageY - e.y;
+      setScreenY(t);
+      if (t > 800) {
+        setH(20);
+        setOp(0);
+      } else if (t <= 800) {
+        setH(400);
+        setOp(1);
+      }
+    });
+  }, []);
   return (
     <div
       style={{
@@ -9,12 +35,47 @@ const SecondArea: FC = () => {
         width: '100%',
         height: '80vh',
         backgroundColor: '#882222',
+        position: 'relative',
       }}
     >
       {/* ここに第２のエリアを挟む */}
+        <animated.div
+          style={{
+            position: 'absolute',
+            width: '13vw',
+            left: '25vw',
+            top: 190,
+            backgroundColor: '#999999',
+            height: styles.height,
+            opacity: styles.opacity,
+          }}
+        />
+        <animated.div
+          style={{
+            position: 'absolute',
+            width: '13vw',
+            left: '40vw',
+            top: 50,
+            backgroundColor: '#999999',
+            height: styles.height,
+            opacity: styles.opacity,
+          }}
+        />
+        <animated.div
+          style={{
+            position: 'absolute',
+            width: '13vw',
+            left: '55vw',
+            top: 110,
+            backgroundColor: '#999999',
+            height: styles.height,
+            opacity: styles.opacity,
+          }}
+        />
+
       <span
         style={{
-          position: 'relative',
+          position: 'absolute',
           top: 120,
           left: '60vw',
           fontSize: 48,
@@ -25,15 +86,16 @@ const SecondArea: FC = () => {
       </span>
       <span
         style={{
-          position: 'relative',
+          position: 'absolute',
           top: 180,
-          left: '30vw',
-          fontSize: 34,
+          left: '60vw',
+          fontSize: 32,
           color: '#ffffff',
         }}
       >
         青であり、蒼であり、碧でもある。
       </span>
+
     </div>
   );
 };
