18f4cec85aaf54fc2f92dde7ed4413a0bf6c9b99
Pregum <yasushi.hino@i-standard.jp>
2022-05-20 15:32:55 +0900

    wip: 4段目ぐらいまで完了

--- src/pages/ForthArea.tsx
+++ src/pages/ForthArea.tsx
@@ -1,22 +1,51 @@
-import { FC, Suspense } from 'react';
+
+import { FC, Suspense, useState, useEffect } from 'react';
+import { animated, config, useSpring, useTransition } from 'react-spring';
 
 const ForthArea: FC = () => {
+  const [screenY, setScreenY] = useState<number>(0);
+
+  const [h, setH] = useState<number>(30);
+  const [op, setOp] = useState<number>(1);
+
+  useEffect(() => {
+    window.addEventListener('wheel', (e) => {
+      console.log('on wheel....e: ', e.screenY, 'offsetY: ', e.offsetY);
+      const t = e.pageY - e.y;
+      setScreenY(t);
+      if (t > 2200) {
+        setH(20);
+        setOp(0);
+      } else if (t <= 2200) {
+        setH(400);
+        setOp(1);
+      }
+    });
+  }, []);
+
+  const styles = useSpring({
+    to: {
+      height: h,
+      opacity: op,
+    },
+  });
+
   return (
     <div
       style={{
         display: 'flex',
-        // top: '100vh',
+        position: 'relative',
         width: '100%',
         height: '80vh',
-        backgroundColor: '#224444',
+        backgroundColor: '#222222',
       }}
     >
       {/* ここに第２のエリアを挟む */}
       <span
         style={{
-          position: 'relative',
+          position: 'absolute',
           top: 120,
-          left: '60vw',
+          left: '70vw',
           fontSize: 34,
           color: '#ffffff',
         }}
@@ -25,15 +54,39 @@ const ForthArea: FC = () => {
       </span>
       <span
         style={{
-          position: 'relative',
+          position: 'absolute',
           top: 180,
-          left: '48vw',
+          left: '70vw',
           fontSize: 34,
           color: '#ffffff',
         }}
       >
-        美味しそう。
+        美味しそう。 -- {screenY}
       </span>
+      <div style={{ display: 'Flex' }}>
+        <animated.div
+          style={{
+            position: 'absolute',
+            width: '15vw',
+            left: '12vw',
+            top: 50,
+            backgroundColor: '#999999',
+            height: styles.height,
+            opacity: styles.opacity,
+          }}
+        />
+        <animated.div
+          style={{
+            position: 'absolute',
+            width: '15vw',
+            left: '28vw',
+            top: 150,
+            backgroundColor: '#999999',
+            height: styles.height,
+            opacity: styles.opacity,
+          }}
+        />
+      </div>
     </div>
   );
 };
--- src/pages/ThirdArea.tsx
+++ src/pages/ThirdArea.tsx
@@ -1,6 +1,34 @@
-import { FC, Suspense } from 'react';
+import { FC, Suspense, useEffect, useState } from 'react';
+import { animated, useSpring } from 'react-spring';
 
 const ThirdArea: FC = () => {
+  const [screenY, setScreenY] = useState<number>(0);
+
+  const [h, setH] = useState<number>(30);
+  const [op, setOp] = useState<number>(1);
+
+  useEffect(() => {
+    window.addEventListener('wheel', (e) => {
+      console.log('on wheel....e: ', e.screenY, 'offsetY: ', e.offsetY);
+      const t = e.pageY - e.y;
+      setScreenY(t);
+      if (t > 1500) {
+        setH(20);
+        setOp(0);
+      } else if (t <= 1500) {
+        setH(400);
+        setOp(1);
+      }
+    });
+  }, []);
+
+  const styles = useSpring({
+    to: {
+      height: h,
+      opacity: op,
+    },
+  });
+
   return (
     <div
       style={{
@@ -9,14 +37,63 @@ const ThirdArea: FC = () => {
         width: '100%',
         height: '80vh',
         backgroundColor: '#448822',
+        position: 'relative',
       }}
     >
-      {/* ここに第２のエリアを挟む */}
+      <div style={{ display: 'flex' }}>
+        <animated.div
+          style={{
+            position: 'absolute',
+            width: '14vw',
+            left: '50vw',
+            top: 120,
+            backgroundColor: '#999999',
+            height: styles.height,
+            opacity: styles.opacity,
+          }}
+        />
+        <animated.div
+          style={{
+            position: 'absolute',
+            width: '14vw',
+            left: '65vw',
+            top: 40,
+            backgroundColor: '#999999',
+            height: styles.height,
+            opacity: styles.opacity,
+          }}
+        />
+        <animated.div
+          style={{
+            position: 'absolute',
+            width: '6vw',
+            left: '48vw',
+            top: 50,
+            backgroundColor: '#99ff99',
+            // height: styles.height,
+            height: 120,
+            opacity: styles.opacity,
+          }}
+        />
+        <animated.div
+          style={{
+            position: 'absolute',
+            width: '6vw',
+            left: '76vw',
+            top: 340,
+            backgroundColor: '#2200aa',
+            // height: styles.height,
+            height: 120,
+            opacity: styles.opacity,
+          }}
+        />
+      </div>
+
       <span
         style={{
-          position: 'relative',
-          top: 120,
-          left: '10vw',
+          position: 'absolute',
+          top: 320,
+          left: '24vw',
           fontSize: 34,
           color: '#ffffff',
         }}
@@ -25,9 +102,9 @@ const ThirdArea: FC = () => {
       </span>
       <span
         style={{
-          position: 'relative',
-          top: 180,
-          left: '-10vw',
+          position: 'absolute',
+          top: 380,
+          left: '24vw',
           fontSize: 34,
           color: '#ffffff',
         }}
