d80478e1f061f094b302efcea7a9a16d8cc30a40
Pregum <yasushi.hino@i-standard.jp>
2022-05-20 10:30:21 +0900

    wip: 1パネルを作成

--- src/pages/Box.tsx
+++ src/pages/Box.tsx
@@ -6,19 +6,18 @@ const Box = (props: JSX.IntrinsicElements['mesh']) => {
   const [hovered, hover] = useState(false);
   const [clicked, click] = useState(false);
   useFrame((state, delta) => {
-    ref.current.rotation.x += 0.01;
-    ref.current.rotation.y += 0.05;
+    ref.current.rotation.y += 0.005;
   });
   return (
     <mesh
       {...props}
       ref={ref}
-      scale={clicked ? 1.5 : 1}
+      scale={clicked ? 0.5 : 1}
       onClick={(event) => click(!clicked)}
       onPointerOver={(event) => hover(true)}
       onPointerOut={(event) => hover(false)}
     >
-      <boxGeometry args={[1, 1, 1]} />
+      <boxGeometry args={[10, 15, 1]} />
       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
     </mesh>
   );
--- src/pages/DrawCanvas.tsx
+++ src/pages/DrawCanvas.tsx
@@ -5,13 +5,22 @@ import Box from "./Box";
 
 const DrawCanvas: FC = () => {
   return (
+    <div style={{height: '500vh', display: 'flex', width: '100wh', backgroundColor: '#444488', overflow: 'scroll', flexDirection: 'column'}} >
+      {/* <div style={{ height: 50, width: 50 }}> */}
+      <div style={{ height: 300 }}>
     <Suspense fallback={<span>loading...</span>}>
       <Canvas>
         <PerspectiveCamera makeDefault />
         <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
-        <Box position={[0,0,-2.0]} />
+        <Box position={[0,0,-20.0]} />
       </Canvas>
     </Suspense>
+      </div>
+
+      {/* </div> */}
+
+      <div style={{ display: 'flex', top: '200vh', width: '100wh', backgroundColor: '#882222' }}>ここに第２のエリアを挟む</div>
+    </div>
   );
 };
 
